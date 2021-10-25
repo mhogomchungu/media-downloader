3454cb1cee0cd7cab811e8a6845fafc4d51a305c
wvxwxvw <13194155+wvxwxvw@users.noreply.github.com>
2021-10-25 16:31:03 +0300

    Update ru_RU.ts

--- translations/ru_RU.ts
+++ translations/ru_RU.ts
@@ -338,7 +338,7 @@
     <message>
         <location filename="../src/batchdownloader.cpp" line="468"/>
         <source>Get List From File</source>
-        <translation type="unfinished"></translation>
+        <translation>Получить список из файла</translation>
     </message>
     <message>
         <location filename="../src/batchdownloader.cpp" line="285"/>
@@ -663,7 +663,7 @@
     <message>
         <location filename="../src/batchdownloader.cpp" line="216"/>
         <source>Copy Url</source>
-        <translation type="unfinished"></translation>
+        <translation>Копировать URL</translation>
     </message>
     <message>
         <location filename="../src/batchdownloader.cpp" line="228"/>
@@ -673,7 +673,7 @@
     <message>
         <location filename="../src/batchdownloader.cpp" line="241"/>
         <source>Show Media Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Показать параметры медиа</translation>
     </message>
 </context>
 <context>
@@ -751,12 +751,12 @@
     <message>
         <location filename="../src/playlistdownloader.cpp" line="204"/>
         <source>Copy Url</source>
-        <translation type="unfinished"></translation>
+        <translation>Копировать URL</translation>
     </message>
     <message>
         <location filename="../src/playlistdownloader.cpp" line="721"/>
         <source>This May Take A Very Long Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Это может занять очень много времени</translation>
     </message>
 </context>
 </TS>
