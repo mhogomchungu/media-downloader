8cfa7087fcccd742f53bba06a833a76c9de7c9ad
bovirus <1262554+bovirus@users.noreply.github.com>
2022-05-15 12:05:10 +0200

    Update italian language
    @mhogomchungu 
    
    Please check and update. Thanks.

--- translations/it_IT.ts
+++ translations/it_IT.ts
@@ -548,11 +548,13 @@
     </message>
     <message>
         <source>This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.</source>
-        <translation type="unfinished"></translation>
+        <translation>Questo programma è un software gratuito. 
+Puoi ridistribuirlo e/o modificarlo secondo i termini della GNU General Public License pubblicata dalla Free Software Foundation; la versione 2 della Licenza o (a scelta) qualsiasi versione successiva.</translation>
     </message>
     <message>
         <source>This program is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.</source>
-        <translation type="unfinished"></translation>
+        <translation>Questo programma è distribuito nella speranza che possa essere utile, ma SENZA ALCUNA GARANZIA; senza nemmeno la garanzia implicita di COMMERCIABILITÀ o IDONEITÀ PER UNO SCOPO PARTICOLARE.
+Per maggiori dettagli vedi la GNU General Public License .</translation>
     </message>
 </context>
 <context>
