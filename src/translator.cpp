aad1f4536737fdc9e0cc3f49b6b0a52dfe059067
Ahmed Deria <ahmedderia0@gmail.com>
2024-05-04 18:03:09 +0300

    Add arabic translation

--- src/translator.cpp
+++ src/translator.cpp
@@ -38,6 +38,7 @@ translator::translator( settings& s,QApplication& app ) : m_qapp( app ),m_settin
 	this->addString( QObject::tr( "German (Germany)" ),"German (Germany)","de_DE" ) ;
 	this->addString( QObject::tr( "Portuguese (Brazil)" ),"Portuguese (Brazil)","pt_BR" ) ;
 	this->addString( QObject::tr( "Dutch (Netherlands)" ),"Dutch (Netherlands)","nl_NL" ) ;
+	this->addString( QObject::tr( "Arabic" ),"Arabic","ar" ) ;
 	this->setDefaultLanguage() ;
 }
 
