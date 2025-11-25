46ae40bf9b7af769845c392ccbec813caa885581
mhogomchungu <mhogomchungu@gmail.com>
2025-11-25 22:34:42 +0300

    be more precise what version of windows supports the latest version of deno

--- src/utility.cpp
+++ src/utility.cpp
@@ -233,7 +233,26 @@ bool utility::platformisLegacyWindows()
 	}else{
 		const auto m = QOperatingSystemVersion::current() ;
 
-		return m < QOperatingSystemVersion::Windows10 ;
+		if( m.majorVersion() < 10 ){
+
+			return true ;
+
+		}else if( m.majorVersion() == 10 ){
+
+			/*
+			 * Windows 10 (1903)       10.0.18362
+			 * Windows 10 (1809)       10.0.17763
+			 * Windows 10 (1803)       10.0.17134
+			 * Windows 10 (1709)       10.0.16299
+			 * Windows 10 (1703)       10.0.15063
+			 * Windows 10 (1607)       10.0.14393
+			 * Windows 10 (1511)       10.0.10586
+			 * Windows 10              10.0.10240
+			 */
+			return m.microVersion() < 16299 ;
+		}else{
+			return false ;
+		}
 	}
 }
 
